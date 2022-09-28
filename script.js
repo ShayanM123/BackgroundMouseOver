6d5ffff6b0d80b2f3e46a614af19b0387ea348cc
ShayanM123 <111317112+ShayanM123@users.noreply.github.com>
2022-09-28 17:22:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mouseOver(){
+    var a = document.getElementById("main")
+    a.style.backgroundColor = "red"
+}
+
+function mouseOut(){
+    var a = document.getElementById("main")
+    a.style.backgroundColor = "blue"
+}
